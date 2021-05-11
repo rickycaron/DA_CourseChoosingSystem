c034bb815186d705155ca8896087499a0dbedd5f
ShuaiZhang-kul <shuai.zhang@student.kuleuven.be>
2021-05-11 18:55:58 +0200

    setting stateful bean

---
+++ src/main/java/com/a20da10/controller/Login.java
@@ -0,0 +1,30 @@
+package com.a20da10.controller;
+
+import com.a20da10.service.spring.StudentSelfService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
+
+@Controller
+public class Login {
+
+    @Autowired
+    private StudentSelfService studentSelfService;
+
+    @RequestMapping("/login")
+    public String Login(){
+
+    //1.Verification
+    //2.Add studentId into service
+        studentSelfService.setStudentId(1);
+    //3.redirect to home page
+        System.out.println(studentSelfService);
+        return "success";
+    }
+    @RequestMapping("/")
+    public String welcome(){
+        return "index";
+    }
+}
---
+++ src/main/java/com/a20da10/service/spring/StudentGeneralService.java
@@ -0,0 +1,4 @@
+package com.a20da10.service.spring;
+
+public interface StudentGeneralService {
+}
