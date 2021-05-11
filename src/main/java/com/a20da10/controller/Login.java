64c5b86a7bca7a87ace0cc868a6b72dfc8b6d777
ShuaiZhang-kul <shuai.zhang@student.kuleuven.be>
2021-05-11 19:53:00 +0200

    setting stateful bean

--- src/main/java/com/a20da10/controller/Login.java
+++ src/main/java/com/a20da10/controller/Login.java
@@ -5,8 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.annotation.Resource;
-
 @Controller
 public class Login {
 
@@ -25,6 +23,6 @@ public class Login {
     }
     @RequestMapping("/")
     public String welcome(){
-        return "index";
+        return "home";
     }
 }
--- src/main/webapp/WEB-INF/pages/home.jsp
+++ src/main/webapp/WEB-INF/pages/home.jsp
@@ -5,8 +5,7 @@
     <title>WELCOME</title>
 </head>
 <body>
-<h1><%= "Hello World!" %>
-</h1>
+
 <br/>
 <a href="hello-servlet">Hello Servlet</a>
 </body>
