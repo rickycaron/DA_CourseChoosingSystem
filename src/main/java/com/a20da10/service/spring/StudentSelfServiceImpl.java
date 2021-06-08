78a30e546ab2f3cede1bf96187f70adc0a5c2936
ZhuRui <rui.zhu@student.kuleuven.be>
2021-06-08 13:30:22 +0200

    myinfo & logout

--- src/main/java/com/a20da10/service/spring/StudentSelfServiceImpl.java
+++ src/main/java/com/a20da10/service/spring/StudentSelfServiceImpl.java
@@ -1,20 +1,29 @@
 package com.a20da10.service.spring;
 
 import com.a20da10.Entity.spring.StudentEntity;
+import com.a20da10.dao.spring.StudentDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.WebApplicationContext;
+
+import javax.transaction.Transactional;
 
 @Service
 @Scope(value = WebApplicationContext.SCOPE_SESSION, proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class StudentSelfServiceImpl implements StudentSelfService {
 
     private int StudentId;
-    @Override
-    public StudentEntity getBasicInfo() {
 
-        return null;
+    @Autowired
+    private StudentDao studentDao;
+    @Override
+    @Transactional
+    public StudentEntity getBasicInfo() {
+        return studentDao.getStudentEntity(StudentId);
+
+//         null;
     }
 
     public int getStudentId() {
