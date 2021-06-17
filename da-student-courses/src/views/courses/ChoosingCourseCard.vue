b3ae26c8495cfa10fef0276943d447bcf8bcb0b0
ZhuRui <rui.zhu@student.kuleuven.be>
2021-06-18 00:58:12 +0200

    vue small

---
+++ da-student-courses/src/views/courses/ChoosingCourseCard.vue
@@ -0,0 +1,78 @@
+<template>
+
+  <div class="card my-4" style="width: 17rem;">
+        <img :src = "courseImage" class="card-img-top" alt="A photo for a course">
+
+        <div class="card-body overflow-hidden"  style="height: 13rem" >
+          <h5 class="card-title">{{courseName}}</h5>
+          <br>
+          <p class="card-subtitle">{{teacherName}}</p>
+          Course Id: {{courseID}}
+          <div class="overflow-hidden" style="height: 10rem">
+            <p class="card-text">{{courseDetail}}</p>
+          </div>
+        </div>
+
+        <router-link :to = "{ name: 'CourseDetails', params: {id:this.courseID}}" class="mt-3 mb-3">
+            <button class="btn purple-button-primary " > Details </button>
+        </router-link>
+
+        <button 
+          class="btn purple-button-secondary"
+          :hidden="!ableToDelete"
+         @click="enrollACourse"> 
+         Enroll
+         </button>
+
+      </div>
+
+</template>
+
+
+<script>
+export default {
+    props: ['courseinfo','ableToDelete'],
+    data(){
+        return{
+            courseID : this.courseinfo.courseId,
+            courseName : this.courseinfo.name,
+            courseDetail : this.courseinfo.description,
+            teacherID : this.courseinfo.instructorId,
+            teacherName : this.courseinfo.teacherFirstName +' '+ this.courseinfo.teacherLastName,
+            courseImage:require("../../assets/programminginc.png")
+        }
+    },
+    methods: {
+      enrollACourse(){
+        // this.$emit('delete-a-Course', this.courseID)
+        // if(confirm("Do you really want to disenroll from this course?"))
+            // {
+                console.log("To enroll the course of id"+ this.courseID)
+            //     axios({
+            // url: apiurl,
+            // headers: {"Content-Type":"application/json;charset=utf-8"},
+            // data: userInfo,
+            // method: 'PUT',
+            // withCredentials: true,
+            // crossDomain: true
+            // })
+                axios.post('courseStudent/subscribeCourse/'+ this.courseID)
+                .then(resp => {
+                    console.log("The course is enrolled")
+                    console.log(resp.data)
+                    // this.courses = this.courses.filter(function(course, index, arr)
+                    // { 
+                    //     return course.courseId != courseid;
+                    // });
+                })
+                .catch(error => {console.log(error);})
+            // }
+      }
+    }
+
+
+}
+</script>
+
+<style>
+</style>
