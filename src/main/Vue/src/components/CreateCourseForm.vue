0d97a5cca5a6cfa7571f694651aa3005c333b01b
ShuaiZhang-kul <shuai.zhang@student.kuleuven.be>
2021-06-18 12:25:26 +0200

    add transaction for subscribing course

---
+++ src/main/Vue/src/components/CreateCourseForm.vue
@@ -0,0 +1,44 @@
+<template>
+     <form>
+       <label >Email:</label>
+       <input type="email" required v-model="email">
+
+       <label >Password:</label>
+       <input type="password" required v-model="password">
+
+       <label >Role:</label>
+       <select v-model="role">
+           <option value="developer">Web Developer</option>
+           <option value="designer">Web Designer</option>
+       </select>
+
+       <div>
+           <input type="checkbox" required v-model="terms">
+           <label>Accept terms and conditions</label>
+       </div>
+
+
+   </form>
+   
+</template>
+ export default {
+     data(){
+         return{
+             email: ' ',
+             password :' ',
+             role: 'developer',
+             terms:false
+         }
+     }
+ 
+ }
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
